3d5818609d80e701df705e5e4731db4678b8915a
Victor Zuluaga Ramirez <49622776+VictorZ94@users.noreply.github.com>
2025-01-28 19:41:22 -0500

    new changes

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-def suma(a,b):
-    return a+b 
+def suma(a, b):
+    return a+b
 
-def resta(a,b):
+
+def resta(a, b):
     return a-b
+
 
 def mul(a, b):
     return a*b
+
+
+def div(a, b):
+    return a/b
